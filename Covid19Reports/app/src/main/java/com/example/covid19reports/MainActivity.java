6c9ae11f956931c223be1741f399259aadce2a94
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-17 11:15:29 +0530

    retrofit second

--- Covid19Reports/app/src/main/java/com/example/covid19reports/MainActivity.java
+++ Covid19Reports/app/src/main/java/com/example/covid19reports/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.covid19reports;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.covid19reports.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
     }
 }
