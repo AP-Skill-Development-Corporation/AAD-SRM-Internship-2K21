0216188d6d9f66979db929ce5683a3e792b5d65a
chaitanyak963 <you@example.com>
2021-06-25 11:05:35 +0530

    25-06-2021

--- ExampleFirebase/app/src/main/java/com/example/examplefirebase/HomeActivity.java
+++ ExampleFirebase/app/src/main/java/com/example/examplefirebase/HomeActivity.java
@@ -1,18 +1,30 @@
 package com.example.examplefirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.examplefirebase.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class HomeActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home);
+        auth = FirebaseAuth.getInstance();
+        binding.textView.setText("Welcome "+auth.getCurrentUser().getEmail());
     }
 
     public void signout(View view) {
+        auth.signOut();
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
