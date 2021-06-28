408f6dcfe57b4dcc7e13160f9fb4d083f4bc5251
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-28 12:08:17 +0530

    firedb project row design file changes

--- MyFireBaseDB/app/src/main/java/com/example/myfirebasedb/MyAdapter.java
+++ MyFireBaseDB/app/src/main/java/com/example/myfirebasedb/MyAdapter.java
@@ -4,9 +4,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -31,6 +39,32 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.t_name.setText(list.get(position).getName());
         holder.t_email.setText(list.get(position).getEmail());
         holder.t_mobile.setText(list.get(position).getMobile());
+        holder.t_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference reference= FirebaseDatabase.getInstance()
+                        .getReference();
+                Query query=reference.child("student").orderByChild("mobile")
+                        .equalTo(list.get(position).getMobile());
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds:snapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(ctx, "removed", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+               // reference.child(list.get(position).getName()).removeValue();
+
+            }
+        });
     }
 
     @Override
@@ -39,13 +73,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView t_name, t_email, t_mobile;
+        TextView t_name, t_email, t_mobile,t_edit,t_delete;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             t_email = itemView.findViewById(R.id.tv_email);
             t_name = itemView.findViewById(R.id.tv_name);
             t_mobile = itemView.findViewById(R.id.tv_mobile);
+            t_edit= itemView.findViewById(R.id.tv_edit);
+            t_delete = itemView.findViewById(R.id.tv_delete);
         }
     }
 }
