7416d33bcacd9262111c5571989cd9dcc723178e
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-28 10:11:28 +0530

    firedb project

---
+++ MyFireBaseDB/app/src/main/java/com/example/myfirebasedb/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirebasedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myfirebasedb.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+
+    public void saveData(View view) {
+    }
+
+    public void retriveData(View view) {
+    }
+}
