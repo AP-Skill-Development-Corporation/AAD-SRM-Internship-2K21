d3e6922c7142a35202270bd36671a00ee005fd86
muneiah tellakula <muneiahtellakula@gmail.com>
2021-07-09 16:45:16 +0530

    dependable spinner and Retrofit second example

--- MyFireBaseDB/app/src/main/java/com/example/myfirebasedb/MainActivity.java
+++ MyFireBaseDB/app/src/main/java/com/example/myfirebasedb/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.myfirebasedb.databinding.ActivityMainBinding;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,8 +28,23 @@ StudentModel model;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdView adView = new AdView(this);
+
+        adView.setAdSize(AdSize.BANNER);
+
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+      //  mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        binding.adView.loadAd(adRequest);
+
     database=FirebaseDatabase.getInstance();
     reference=database.getReference("student");
+
     }
 
     public void saveData(View view) {
